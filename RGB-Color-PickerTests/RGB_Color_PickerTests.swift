d5ecddd342a7d2126870cf56b3dcb32e9f8c1c9b
FrankAnger <thefrankanger@gmail.com>
2025-10-03 22:34:27 +0300

    Initial Commit

---
+++ RGB-Color-PickerTests/RGB_Color_PickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RGB_Color_PickerTests.swift
+//  RGB-Color-PickerTests
+//
+//  Created by Constantine Grinko on 03.10.2025.
+//
+
+import Testing
+@testable import RGB_Color_Picker
+
+struct RGB_Color_PickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
